1a8df46592ab29329bc0b5fb8c00d69e6227a268
grzegorz <michongrzegorz9@gmail.com>
2017-03-20 19:24:30 +0100

    main-window3+pracownicy1+zadania1

--- MainWindow/src/mainwindow/FXMLDocumentController.java
+++ MainWindow/src/mainwindow/FXMLDocumentController.java
@@ -29,7 +29,11 @@ public class FXMLDocumentController implements Initializable {
     private Button app_info;
     @FXML  
     private Button settings;
-   
+    @FXML  
+    private Button add_task;
+    @FXML  
+    private Button add_employee;
+    
     @FXML
     private void showInfoWindow(ActionEvent event) throws IOException {
         Parent loader = FXMLLoader.load(getClass().getResource("InfoAplication.fxml"));
@@ -44,23 +48,45 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private void showHotelInfoWindow(ActionEvent event) throws IOException {
         Parent loader = FXMLLoader.load(getClass().getResource("HotelInfo.fxml"));
-        Scene info_scene= new Scene(loader);
-        Stage info_stage =new Stage();
-        info_stage.setScene(info_scene);
-        info_stage.initModality(Modality.APPLICATION_MODAL);
-        info_stage.initOwner(hotel_info.getScene().getWindow());
-        info_stage.showAndWait();
+        Scene info2_scene= new Scene(loader);
+        Stage info2_stage =new Stage();
+        info2_stage.setScene(info2_scene);
+        info2_stage.initModality(Modality.APPLICATION_MODAL);
+        info2_stage.initOwner(hotel_info.getScene().getWindow());
+        info2_stage.showAndWait();
     }
     
     @FXML
     private void showSettingsWindow(ActionEvent event) throws IOException {
         Parent loader = FXMLLoader.load(getClass().getResource("Settings.fxml"));
-        Scene info_scene= new Scene(loader);
-        Stage info_stage =new Stage();
-        info_stage.setScene(info_scene);
-        info_stage.initModality(Modality.APPLICATION_MODAL);
-        info_stage.initOwner(settings.getScene().getWindow());
-        info_stage.showAndWait();
+        Scene settings_scene= new Scene(loader);
+        Stage settings_stage =new Stage();
+        settings_stage.setScene(settings_scene);
+        settings_stage.initModality(Modality.APPLICATION_MODAL);
+        settings_stage.initOwner(settings.getScene().getWindow());
+        settings_stage.showAndWait();
+    }
+    
+    @FXML
+    private void add_taskwindow(ActionEvent event) throws IOException {
+        Parent loader = FXMLLoader.load(getClass().getResource("add_task.fxml"));
+        Scene task_scene= new Scene(loader);
+        Stage task_stage =new Stage();
+        task_stage.setScene(task_scene);
+        task_stage.initModality(Modality.APPLICATION_MODAL);
+        task_stage.initOwner(add_task.getScene().getWindow());
+        task_stage.showAndWait();
+    }
+    
+    @FXML
+    private void add_employeewindow(ActionEvent event) throws IOException {
+        Parent loader = FXMLLoader.load(getClass().getResource("add_employee.fxml"));
+        Scene emp_scene= new Scene(loader);
+        Stage emp_stage =new Stage();
+        emp_stage.setScene(emp_scene);
+        emp_stage.initModality(Modality.APPLICATION_MODAL);
+        emp_stage.initOwner(add_employee.getScene().getWindow());
+        emp_stage.showAndWait();
     }
     
     @Override
